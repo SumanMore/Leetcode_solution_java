533493020e34b3e4c0749555d8be665e32a424a7
Suman More <67056908+SumanMore@users.noreply.github.com>
2023-02-20 12:21:52 +0530

    Update 9. Palindrome Number.java

--- 9. Palindrome Number.java
+++ 9. Palindrome Number.java
@@ -11,6 +11,29 @@ class Solution {
             j--;                               
         }
        return true;
-    
+    }
+}
+
+
+class Solution {
+    public boolean isPalindrome(int x) {
+       String s= Integer.toString(x);
+        int i=0;
+        boolean ans=false;
+        if(s.charAt(0)=='-')
+        {
+         i=1;
+         return ans;
+        }
+        String res="";
+        for(int k=s.length()-1;k>=0;k--)
+        {
+          res=res+s.charAt(k);
+        }
+        if(res.equals(s))
+        {
+            ans=true;
+        }
+    return ans;
     }
 }

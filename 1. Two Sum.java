3558140394fa4cbef44ecc47a22c429db1b53740
Suman More <67056908+SumanMore@users.noreply.github.com>
2023-02-20 11:58:49 +0530

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -23,3 +23,26 @@ public class Main
         }
     }
 }    
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) 
+    {
+        int res[]=new int[2];
+        int c=0;
+        HashMap<Integer,Integer> ob=new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(ob.containsKey(target-nums[i]))
+            {
+                res[1]=i;
+                res[0]=ob.get(target-nums[i]);
+                
+            }
+            ob.put(nums[i],i);
+        }
+        
+        return res;
+        
+    }
+}

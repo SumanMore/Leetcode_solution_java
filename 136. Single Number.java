d70e069425d3de02c7dd1e3287f501e50202fc42
Suman More <67056908+SumanMore@users.noreply.github.com>
2023-02-22 20:09:51 +0530

    Create 136. Single Number.java

---
+++ 136. Single Number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int singleNumber(int[] nums)
+    {
+    int c=0;
+    int res=0;
+      for(int i=0;i<nums.length;i++)
+      { 
+          c=0;
+          for(int j=0;j<nums.length;j++)
+          {
+           if(nums[i]==nums[j])
+           {
+               c++;
+           }
+          }
+          if(c==1)
+          {
+              res= nums[i];
+          }
+      }  
+      return res;
+    }
+}

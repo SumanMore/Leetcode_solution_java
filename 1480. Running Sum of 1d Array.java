05b293caf10be810320964cb23e7005196e3cbe2
Suman More <67056908+SumanMore@users.noreply.github.com>
2023-03-07 19:41:53 +0530

    Create 1480. Running Sum of 1d Array.java

---
+++ 1480. Running Sum of 1d Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] runningSum(int[] nums) 
+    {
+        int ar[]=new int[nums.length];
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum=sum+nums[i];
+            ar[i]=sum;
+        }
+        return ar;
+        
+    }
+}

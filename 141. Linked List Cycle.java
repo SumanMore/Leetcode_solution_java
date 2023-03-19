5246c883e98e2964b7bbaec72058ce935f831405
Suman More <67056908+SumanMore@users.noreply.github.com>
2023-03-19 22:56:31 +0530

    Create 141. Linked List Cycle.java

---
+++ 141. Linked List Cycle.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slower=head;
+        ListNode faster=head;
+        while(faster!=null && faster.next!=null)
+        {
+            slower=slower.next;
+            faster=faster.next.next;
+            if(slower==faster)
+            {
+                return true;
+            }
+        }
+        return false;
+
+        
+    }
+}

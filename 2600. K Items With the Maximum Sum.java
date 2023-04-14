211a586c5ce47807724d91b4c4794206a88845ae
Suman More <67056908+SumanMore@users.noreply.github.com>
2023-04-14 22:59:16 +0530

    Create 2600. K Items With the Maximum Sum.java

---
+++ 2600. K Items With the Maximum Sum.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) 
+    {
+        return Math.min(k, numOnes) - Math.max(0, k - numOnes - numZeros);
+        
+    }
+}
